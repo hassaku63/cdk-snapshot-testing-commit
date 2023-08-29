4d929dca65c8d32c67ed717560351fc425234e3a
hassaku63 <hassaku63@gmail.com>
2023-08-29 11:59:24 +0900

    Create test resource

--- bin/cdk-snapshot-commit.ts
+++ bin/cdk-snapshot-commit.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkSnapshotCommitStack } from '../lib/cdk-snapshot-commit-stack';
+import { TestStack } from '../lib/test-slack';
 
 const app = new cdk.App();
-new CdkSnapshotCommitStack(app, 'CdkSnapshotCommitStack', {
+new TestStack(app, 'CdkSnapshotCommitStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
---
+++ lib/test-slack.ts
@@ -0,0 +1,37 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as aws_lambda from "aws-cdk-lib/aws-lambda";
+import * as aws_codebuild from "aws-cdk-lib/aws-codebuild";
+
+export class TestStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const func = new aws_lambda.Function(this, "TestFunction", {
+      runtime: aws_lambda.Runtime.NODEJS_18_X,
+      handler: "index.handler",
+      code: aws_lambda.Code.fromInline(`
+        exports.handler = async function (event, context) {
+          console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+          return context.logStreamName;
+        };
+      `),
+    });
+
+    const project = new aws_codebuild.Project(this, "TestProject", {
+      buildSpec: aws_codebuild.BuildSpec.fromObject({
+        version: "0.2",
+        phases: {
+          build: {
+            commands: [
+              "echo hello world",
+            ],
+          },
+        },
+      }),
+    });
+
+    new cdk.CfnOutput(this, "TestFunctionArn", { value: func.functionArn });
+    new cdk.CfnOutput(this, "TestProjectArn", { value: project.projectArn });
+  }
+}
